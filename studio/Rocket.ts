b921c90116b3ad328f51dce78fec25ca798f280a
Sam <samtpomarico@gmail.com>
2020-09-08 19:49:06 -0500

    completed studio

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+import { Payload } from './Payload';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass (items: Payload[]): number {
+        let totalMass: number = 0;
+        for (let i = 0; i < items.length; i++) {
+            totalMass += items[i].massKg;
+        }
+        return totalMass;
+    }
+    currentMassKg(): number {
+        let astronautMass: number = this.sumMass(this.astronauts);
+        let cargoMass: number = this.sumMass(this.cargoItems);
+        let combinedMass: number = astronautMass + cargoMass;
+        return combinedMass;
+    }
+    canAdd(item: Payload): boolean {
+        if(this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if(this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;    
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if(this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
